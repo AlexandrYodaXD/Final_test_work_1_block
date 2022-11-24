bdc63ca16d3d9e623b71f72698d8f4e8c2338cef
Alexandr YodaXD <skyfoxalex@gmail.com>
2022-11-25 00:16:00 +0300

    added method CreateSortedArray

--- Program.cs
+++ Program.cs
@@ -16,3 +16,29 @@ void PrintArray(string[] array) // печать string массива
     Console.WriteLine(" }\n");
 }
 
+string[] СreateSortedArray(string[] array, int requiredLength = 3)
+{
+    string trueIndexes = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= requiredLength)
+        {
+            trueIndexes += $"{i}";
+        }
+    }
+
+    string[] sortedArray = new string[trueIndexes.Length];
+    for (int i = 0; i < sortedArray.Length; i++)
+    {
+        sortedArray[i] = array[int.Parse(trueIndexes[i].ToString())]; // приводим trueIndexes[i] к string, затем полученное значение приводим к int и используем как индекс строки.
+    }
+
+    return sortedArray;
+}
+
+
+
+string[] arr = {"hello", "2", "world", ":-)"};
+
+//PrintArray(arr);
+//PrintArray(СreateSortedArray(arr));
