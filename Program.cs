88b360780126ae4db6e071201cfe65cf3bc8866a
Alexandr YodaXD <skyfoxalex@gmail.com>
2022-11-25 00:14:30 +0300

    added method PrintArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+исключительно массивами.
+*/
+
+void PrintArray(string[] array) // печать string массива
+{
+    Console.Write("{ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) // ставим запятую после элемента, если это не последний элемент
+            Console.Write(", ");
+    }
+    Console.WriteLine(" }\n");
+}
+
